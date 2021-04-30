6f4c83d0024e675e236e8ae43bae1dd19b98057a
anasinikitha <67320294+anasinikitha@users.noreply.github.com>
2021-04-30 16:38:21 +0530

    Create Activity3.h

---
+++ inc/Activity3.h
@@ -0,0 +1,22 @@
+#ifndef __ACTIVITY3_H_
+#define __ACTIVITY3_H_
+/**
+ * @file ACTIVITY1.h
+ * @author Nikitha A ()
+ * @brief Activity3 to see variation of temperature in CRO connected to AVR328 MCU GPIO Pin when heater button is presses
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __AVR_ATmega328__
+  #define __AVR_ATmega328__
+#endif
+
+#include<avr/io.h>
+  
+void InitPwm();
+
+#endif
